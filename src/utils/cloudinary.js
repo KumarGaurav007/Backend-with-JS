106abf64d3340ef0441f65491826aac2b773140c
Gaurav Kumar <kumargaurav1073@gmial.com>
2026-06-04 18:00:10 +0530

    minnor bugs fixed

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -3,11 +3,11 @@ import fs from "fs";
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: CLOUDINARY_API_KEY,
-    api_secret: CLOUDINARY_API_SECRET,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadOnCloudinary = async (localFilePath) => {
+export const uploadOnCloudinary = async (localFilePath) => {
     try {
         if(!localFilePath) return null;
         // upload the file on cloudinary
@@ -23,4 +23,3 @@ const uploadOnCloudinary = async (localFilePath) => {
     }
 }
 
-cloudinary.uploader.upload("")
